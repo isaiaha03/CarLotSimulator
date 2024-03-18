55fe5381a18c1a27270115262b4a7ff2cb28af07
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-03-18 16:14:12 -0500

    Finished exercise and bonuses.

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class CarLot
+    {
+        public List<Car> Cars { get; } = new List<Car>();
+
+        public void AddCar(Car car)
+        {
+            Cars.Add(car);
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,28 +6,63 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            CarLot carLot = new CarLot();
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //DONE: Create a seperate class file called Car
+            //DONE: Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+            //DONE: Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //DONE: The methods should take one string parameter: the respective noise property
 
 
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
+            //DONE: Now that the Car class is created we can instanciate 3 new cars
+            //DONE: Set the properties for each of the cars
+            //DONE: Call each of the methods for each car
+            Car carOne = new Car()
+            {
+                Year = 2020,
+                Make = "Ford",
+                Model = "Mustang",
+                EngineNoise = "Rumble",
+                HonkNoise = "Honk",
+                IsDriveable = true,
+            };
+            carOne.MakeEngineNoise();
+            carOne.MakeHonkNoise();
+            carLot.AddCar(carOne);
+
+            Car carTwo = new Car() { Year = 2022, Make = "Toyota", Model = "Supra", EngineNoise = "Vroom", HonkNoise = "Beep", IsDriveable = true };
+            carTwo.MakeEngineNoise();
+            carTwo.MakeHonkNoise();
+            carLot.AddCar(carTwo);
+
+            Car carThree = new Car();
+            carThree.Year = 2023;
+            carThree.Make = "Chevy";
+            carThree.Model = "Silverado";
+            carThree.EngineNoise = "Roar";
+            carThree.HonkNoise = "Horn";
+            carThree.IsDriveable = true;
+            carThree.MakeEngineNoise();
+            carThree.MakeHonkNoise();
+            carLot.AddCar(carThree);
 
             //*************BONUS*************//
 
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            //DONE: Set the properties utilizing the 3 different ways we learned about, one way for each car
 
             //*************BONUS X 2*************//
 
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            //DONE: Create a CarLot class
+            //DONE: It should have at least one property: a List of cars
+            //DONE: Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+            //DONE: At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            Console.WriteLine("Cars in the car lot:");
+            foreach (Car car in carLot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year}, Make: {car.Make}, Model: {car.Model}");
+            }
         }
     }
 }
